6c96b0bdb7f11a537dde84a327e382e79029935e
Ubuntu <ubuntu@ip-172-31-20-200.ec2.internal>
2026-06-01 10:16:25 +0000

    CI/CD test update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("DevOps CI/CD Working 🚀");
+    res.send("Updated CI/CD Pipeline Working 🚀");
 });
 
 app.listen(3000, () => {
